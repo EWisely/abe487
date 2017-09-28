fc5f8260ee6cdcd8ac47861effb4c5ddbd7887d8
Eldridge Katherine Wisely <eldridgewisely@login2.cm.cluster>
2017-09-28 14:44:56 -0700

    adding unix-utils homework

--- problems/cat-n/cat-n.sh
+++ problems/cat-n/cat-n.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-echo "OK"
+set -u
+
+if [[ $# -ne 1 ]]; then
+	printf "Usage: %s File\n" "$(basename "$0")"
+	exit 1
+fi
+
+i=0
+File=$1
+
+while read -r Line; do
+	let i++
+	echo "$i $Line"
+done < "$File"
