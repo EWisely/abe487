50b6fb199dd791ae57ec347ad643d3ba2c9c4c19
Eldridge Katherine Wisely <eldridgewisely@email.arizona.edu>
2017-09-28 16:28:48 -0700

    all good, thanks explaining where to find the homework assignments, and the little extra time!

--- problems/unix-utils/cat-n.sh
+++ problems/unix-utils/cat-n.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-echo "OK"
+set -u
+
+if [[ $# -ne 1 ]]; then
+	printf "Usage: %s file\n" "$(basename "$0")"
+	exit 1
+fi
+
+file=$1
+           
+if [[ ! -f "$file" ]]; then
+	echo "\"$file\" is not a file"
+	exit 1
+fi
+
+i=0
+
+while read -r Line; do
+	let i++
+	echo "$i $Line"
+done < "$file"
+
--- problems/unix-utils/head.sh
+++ problems/unix-utils/head.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
-echo "OK"
+set -u
+
+NUMBER=${2:-3}
+
+if [[ $# -lt 1 ]] || [[ $# -gt 2 ]]; then 
+	printf "Usage: %s FILE [NUM]\n" "$(basename "$0")"
+	exit 1
+fi
+
+FILE=$1
+if [[ ! -f "$FILE" ]]; then
+        echo "\"$FILE\" is not a file"
+        exit 1
+fi
+
+i=0
+while read -r LINE; do
+	if [[ "$i" -lt "$NUMBER" ]]; then
+	echo $LINE
+	fi
+	let i++
+done < "$FILE"
+
