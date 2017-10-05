ba7b559a96da749c1875482eb07757f5a7f7ba3f
Eldridge Katherine Wisely <eldridgewisely@email.arizona.edu>
2017-10-04 22:24:40 -0700

    hello-py homework complete

--- problems/hello-py/hello.py
+++ problems/hello-py/hello.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
-"""say hello to the arguments"""
+"""
+say hello to the arguments
+@author: Eldridge
+"""
 
-print('OK')
+import sys
+if len(sys.argv)<2:
+    sys.exit("Usage: "+sys.argv[0]+"NAME [NAME2...]")
+elif len(sys.argv)>1:
+	names=sys.argv[1:]
+	if len(names)==1:
+		print ("Hello to the 1 of you: " + sys.argv[1]+ "!")
+	if len(names)>1:
+		names.insert(-1, "and")
+	if len(names)==3:
+		print ("Hello to the 2 of you: " +' '.join(names)+"!") 
+	if len(names)>3:
+		print ("Hello to the " +str(len(sys.argv)-1) + " of you: " + ', '.join(names[0:-1]) + " " + names[-1] +"!")
+
+
--- problems/hello-py/vowel_counter.py
+++ problems/hello-py/vowel_counter.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 """count the vowels in a word"""
 
-print('OK')
+import sys
+if len(sys.argv)==2:
+	word=str(sys.argv[1])
+	vowels="aeiouAEIOU"
+	n=0
+	for letter in word:
+		if letter in vowels:
+			n=n+1
+	#print(n) #works!
+	if n ==1:
+		print("There is 1 vowel in " + '"' + word +'."')
+	else:
+		print ("There are "+ str(n) + " vowels in " + '"'+ word +'."')
+
+elif len(sys.argv)<2 or len(sys.argv)>2:
+	sys.exit("Usage: "+sys.argv[0]+" STRING")
