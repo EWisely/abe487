ba722792204c9ef515de0a108b50b15de6e15751
Eldridge Katherine Wisely <eldridgewisely@email.arizona.edu>
2017-11-30 14:50:31 -0700

    adding last quiz

--- problems/last_quiz/adder.py
+++ problems/last_quiz/adder.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python3
+"""
+add thing 1 and thing 2
+@author: Eldridge
+"""
 
-print('OK')
+import sys
+if len(sys.argv)!=3:
+    sys.exit("Usage: "+sys.argv[0]+" ARG1 ARG2")
+elif len(sys.argv)==3:
+    things=sys.argv[1:]
+    thing1= things[0]
+    thing2 =things[1]
+    if thing1.isdigit() and thing2.isdigit():
+        print (int(thing1)+int(thing2))
+    if thing1.isalpha() and thing2.isalpha():
+        print (thing1 +' and ' +thing2)	
+    else:
+        print ('Cannot combine number and string')
